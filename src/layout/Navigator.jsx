d6a358545d8bcffb654b7758bf60af6ed74b09fe
harshil8000 <baradharshil00@gmail.com>
2024-11-22 20:00:40 +0530

    fieis

--- src/layout/Navigator.jsx
+++ src/layout/Navigator.jsx
@@ -19,7 +19,7 @@ const Navigator = () => {
           {/* <div className="logo">
             <Logo />
           </div> */}
-          <h1>shoes</h1>
+          <h1 style={{fontWeight:"500px"}}>shoes</h1>
           <ul className="nav-links">
             {navLinks.map((link, idx) => {
               return (
